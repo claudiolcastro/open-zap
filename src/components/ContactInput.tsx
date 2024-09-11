6d8b7adfbe7330b342c0cd91886c5cea417cfeae
Claudio Castro <claudioluiz.castro@gmail.com>
2024-09-11 20:55:06 -0300

    feat: update styles and color palet   - add numer format function

--- src/components/ContactInput.tsx
+++ src/components/ContactInput.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./ContactInput.module.css";
 
 export default function ContactInput() {
   const [number, setNumber] = useState("");
@@ -6,47 +7,54 @@ export default function ContactInput() {
 
   const sendNumberToZap = () => {
     if (number) {
-      const url = `https://api.whatsapp.com/send?phone=55${number}&text=${message}`;
+      const url = `https://api.whatsapp.com/send?phone=55${number.replace(
+        /\D/g,
+        ""
+      )}&text=${message}`;
       window.open(url, "_blank").focus();
     }
+  };
+
+  const formatPhoneNumber = (value) => {
+    const cleaned = ("" + value).replace(/\D/g, "");
+
+    if (cleaned.length <= 2) return cleaned;
+    if (cleaned.length <= 6)
+      return `(${cleaned.slice(0, 2)}) ${cleaned.slice(2)}`;
+    return `(${cleaned.slice(0, 2)}) ${cleaned.slice(2, 7)}-${cleaned.slice(
+      7,
+      11
+    )}`;
+  };
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setNumber(formatPhoneNumber(value));
   };
 
   return (
     <div>
       <h2>Inicie uma conversa no WhatsApp sem precisar adicionar um contato</h2>
-      <p>Ex: 21991910000</p>
 
       <input
         type="tel"
         placeholder="Numero do contato"
-        onChange={(e) => setNumber(e.target.value)}
-        style={{ display: "block", padding: "8px", margin: "10px 0" }}
+        value={number}
+        onChange={handleChange}
+        className={styles.numberInput}
       />
 
       <textarea
         placeholder="Sua mensagem (opicional)"
         onChange={(e) => setMessage(e.target.value)}
-        style={{
-          display: "block",
-          padding: "12px",
-          width: "300px",
-          height: "100px",
-        }}
+        className={styles.messageInput}
       />
 
-      <p style={{ maxWidth: "300px", margin: "10px 0" }}>
+      <p style={{ maxWidth: "300px", margin: "10px 0", color: "#075e54" }}>
         Ao clicar, uma conversa no WhatsApp será aberta com o contato
       </p>
 
-      <button
-        onClick={() => sendNumberToZap()}
-        style={{
-          display: "block",
-          padding: "12px 25px",
-          cursor: "pointer",
-          margin: "10px 0",
-        }}
-      >
+      <button onClick={() => sendNumberToZap()} className={styles.button}>
         Conversar
       </button>
     </div>
