f9e59beb1804f214d8cfcc8a799f0ef1ba114ede
Claudio Castro <claudioluiz.castro@gmail.com>
2024-09-11 20:22:37 -0300

    feat: remove images and update subtitle

--- src/components/ContactInput.tsx
+++ src/components/ContactInput.tsx
@@ -1,45 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function ContactInput() {
-  const [number, setNumber] = useState('')
-  const [message, setMessage] = useState('')
+  const [number, setNumber] = useState("");
+  const [message, setMessage] = useState("");
 
   const sendNumberToZap = () => {
     if (number) {
-      const url = `https://api.whatsapp.com/send?phone=55${number}&text=${message}`
-      window.open(url, '_blank').focus()
+      const url = `https://api.whatsapp.com/send?phone=55${number}&text=${message}`;
+      window.open(url, "_blank").focus();
     }
-  }
+  };
 
   return (
     <div>
-      <h2>Bota aí um contato (BR) com DDD</h2>
+      <h2>Inicie uma converca no whatsapp sem precisar adicionar um contato</h2>
       <p>Ex: 21991910000</p>
 
       <input
         type="tel"
         placeholder="contatinho"
         onChange={(e) => setNumber(e.target.value)}
-        style={{ display: 'block', padding: '8px', margin: '10px 0' }}
+        style={{ display: "block", padding: "8px", margin: "10px 0" }}
       />
 
       <textarea
-        type="text"
         placeholder="meteu essa?"
         onChange={(e) => setMessage(e.target.value)}
-        style={{ display: 'block', padding: '12px', width: '300px', height: '100px' }}
+        style={{
+          display: "block",
+          padding: "12px",
+          width: "300px",
+          height: "100px",
+        }}
       />
 
-      <p style={{ maxWidth: '300px', margin: '10px 0' }}>
-        Seguinte, tu vai mandar mensagem no <b>ZAP</b> sem precisar add o contato, sacou?
+      <p style={{ maxWidth: "300px", margin: "10px 0" }}>
+        Seguinte, tu vai mandar mensagem no <b>ZAP</b> sem precisar add o
+        contato, sacou?
       </p>
 
       <button
         onClick={() => sendNumberToZap()}
-        style={{ display: 'block', padding: '12px 25px', cursor: 'pointer', margin: '10px 0' }}
+        style={{
+          display: "block",
+          padding: "12px 25px",
+          cursor: "pointer",
+          margin: "10px 0",
+        }}
       >
         VAI!
       </button>
     </div>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import ContactInput from '../components/ContactInput'
-import RandomCaze from '../components/RandomCaze'
-import styles from '../styles/Home.module.css'
+import ContactInput from "../components/ContactInput";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Open Zap</h1>
 
-      <RandomCaze />
       <ContactInput />
     </div>
-  )
+  );
 }
